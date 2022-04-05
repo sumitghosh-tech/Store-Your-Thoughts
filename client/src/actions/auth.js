4b195917f0565e785003a7612f3c5baccb805d9e
Sumit Kumar Ghosh <64772108+sumitghosh-tech@users.noreply.github.com>
2022-04-05 20:08:13 +0530

    Update auth.js

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,9 +5,8 @@ import * as api from '../api';
 
 export const signin = (formData, navigate) => async (dispatch) => {
   try {
-    const { data } = await api.signIn(formData);  //data=[result and tokden] which is given by server\controllers\user.js ->signin function
-    //console.log("sd",data);
-    dispatch({ type: "AUTH", data });  //saving data in localstorage by reducers
+    const { data } = await api.signIn(formData);  
+    dispatch({ type: "AUTH", data });  
 
     navigate("/");
   } catch (error) {
@@ -17,8 +16,7 @@ export const signin = (formData, navigate) => async (dispatch) => {
 
 export const signup = (formData, navigate) => async (dispatch) => {
   try {
-    const { data } = await api.signUp(formData);  //data=[result and token] which is given by server\controllers\user.js ->signup function
-
+    const { data } = await api.signUp(formData);  /
     dispatch({ type: "AUTH", data });
 
     navigate('/');
