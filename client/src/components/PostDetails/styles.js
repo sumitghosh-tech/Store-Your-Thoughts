286e4bcf4c8a8abb03b886a203a1e0ea0a8928ab
Sumit Kumar Ghosh <64772108+sumitghosh-tech@users.noreply.github.com>
2022-04-05 20:11:16 +0530

    Update styles.js

--- client/src/components/PostDetails/styles.js
+++ client/src/components/PostDetails/styles.js
@@ -8,31 +8,7 @@ export default makeStyles((theme) => ({
     height: '80%',
 
   },
-  // card: {
-  //   display: 'flex',
-  //   width: '100%',
-  //   [theme.breakpoints.down('sm')]: {
-  //     flexWrap: 'wrap',
-  //     flexDirection: 'column',
-  //   },
-  // },
-  // section: {
-  //   borderRadius: '20px',
-  //   margin: '10px',
-  //   flex: 1,
-  // },
-  // imageSection: {
-  //   marginLeft: '20px',
-  //   [theme.breakpoints.down('sm')]: {
-  //     marginLeft: 0,
-  //   },
-  // },
-  // recommendedPosts: {
-  //   display: 'flex',
-  //   [theme.breakpoints.down('sm')]: {
-  //     flexDirection: 'column',
-  //   },
-  // },
+  
   loadingPaper: {
     display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px', borderRadius: '15px', height: '39vh',
   },
