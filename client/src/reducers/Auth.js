6f8afbca9b44d566bb41e1963859ae9f20672805
Sumit Kumar Ghosh <64772108+sumitghosh-tech@users.noreply.github.com>
2022-04-05 20:07:28 +0530

    g

--- client/src/reducers/Auth.js
+++ client/src/reducers/Auth.js
@@ -1,9 +1,7 @@
 const authReducer = (state = null, action) => {
     switch (action.type) {
       case "AUTH":
-        localStorage.setItem('profile', JSON.stringify(action?.data));//saving to localstorage for staying logged in
-        //localstorage e profile name er akta item create kore setate action?.data store kora holo
-
+        localStorage.setItem('profile', JSON.stringify(action?.data));
         return state=action?.data;
       case "LOGOUT":
         localStorage.clear();
@@ -16,28 +14,3 @@ const authReducer = (state = null, action) => {
   
   export default authReducer;
 
-//JSON.stringify -> storing
-
-//JSON.parse -> saving
-
-
-
-
-/*
-const authReducer = (state = { authData: null }, action) => {
-    switch (action.type) {
-      case "AUTH":
-        localStorage.setItem('profile', JSON.stringify({ ...action?.data }));//saving to localstorage for staying logged in
-        //localstorage e profile name er akta item create kore setate action?.data store kora holo
-        return { ...state, authData: action?.data, loading: false, errors: null };
-      case "LOGOUT":
-        localStorage.clear();
-  
-        return { ...state, authData: null, loading: false, errors: null };
-      default:
-        return state;
-    }
-  };
-  
-  export default authReducer;
-*/
