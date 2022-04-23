3dd757c9a6cac056489209321a1af0a4a5ca46ed
Sumit Kumar Ghosh <64772108+sumitghosh-tech@users.noreply.github.com>
2022-04-23 13:07:57 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,17 +6,14 @@ const API=axios.create({baseURL:"BASE_URL"});
 
 
 export const fetchPosts=()=>API.get("/posts");    
+
 export const fetchPost=(id)=>API.get(`/posts/${id}`); 
 
 export const createPost=(newPost)=>API.post("/posts",newPost);  
 
 export const updatePost=(id,updatedPost)=>API.patch(`/posts/${id}`,updatedPost);  
 
-
 export const deletePost=(id)=>API.delete(`/posts/${id}`);
-
-export const likePost=(id)=>API.patch(`/posts/${id}/likePost`);
-
 
 export const signIn=(formData)=>API.post("/user/signin",formData);
 
