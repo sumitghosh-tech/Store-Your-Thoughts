53ab7d48be176b6558f1dc95ed85dae93f03e399
Sumit Kumar Ghosh <64772108+sumitghosh-tech@users.noreply.github.com>
2022-04-23 13:04:14 +0530

    Update user.js

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 
 import UserModal from "../models/user.js";
 
-const secret = 'test';
+const secret = 'Secret here';
 
 export const signin = async (req, res) => {
   const { email, password } = req.body;
